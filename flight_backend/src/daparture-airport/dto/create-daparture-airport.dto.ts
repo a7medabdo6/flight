dd9e1b25e2f320035a7c6e81f52f10fea8ea6981
a7medabdo6 <57114105+a7medabdo6@users.noreply.github.com>
2023-03-29 19:19:30 +0200

    Update create-daparture-airport.dto.ts

--- flight_backend/src/daparture-airport/dto/create-daparture-airport.dto.ts
+++ flight_backend/src/daparture-airport/dto/create-daparture-airport.dto.ts
@@ -3,6 +3,11 @@ import { IsNumber, IsString } from "class-validator";
 export class CreateDapartureAirportDto {
     @IsString()
     name:string
+    
+    @IsString()
+    country:string
+    @IsString()
+    city:string
     @IsNumber()
     city_id:number
 }
