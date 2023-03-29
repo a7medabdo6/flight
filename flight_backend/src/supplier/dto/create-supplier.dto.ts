889560b4fc605d2ad6b45c6f6413ddf45b8992d5
a7medabdo6 <57114105+a7medabdo6@users.noreply.github.com>
2023-03-29 19:23:57 +0200

    Update create-supplier.dto.ts

--- flight_backend/src/supplier/dto/create-supplier.dto.ts
+++ flight_backend/src/supplier/dto/create-supplier.dto.ts
@@ -3,4 +3,9 @@ import { IsString } from 'class-validator';
 export class CreateSupplierDto {
   @IsString()
   name: string;
+  
+  @IsString()
+  country: string;
+  @IsString()
+  city: string;
 }
